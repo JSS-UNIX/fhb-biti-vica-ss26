6dd5170c5811d03e0e824e27cff288577124ee43
Johannes Schurian <2410640035@hochschule-burgenland.at>
2026-06-06 20:28:00 +0200

    created main.tf and cloud-init.yml

--- Schurian_Johannes/Abgabe_2_TERRAFORM/main.tf
+++ Schurian_Johannes/Abgabe_2_TERRAFORM/main.tf
@@ -6,31 +6,31 @@ terraform {
     }
   }
 }
- 
+
 # Provider Konfiguration bleibt leer.
 # Terraform nutzt stattdessen automatisch die Umgebungsvariablen 
 # EXOSCALE_API_KEY und EXOSCALE_API_SECRET (z.B. aus GitHub Actions).
 provider "exoscale" {
 }
- 
+
 locals {
-  zone       = "ch-gva-2"                       
+  zone       = "at-vie-1"  # Zone in Wien, Österreich                 
   template   = "Linux Ubuntu 26.04 LTS 64-bit"  
 }
- 
+
 # 1. Das aktuellste Ubuntu-Template in der gewählten Zone suchen
-data "exoscale_compute_template" "ubuntu" {
+data "exoscale_template" "ubuntu" {
   zone = local.zone
   name = local.template
 }
- 
+
 # 2. Security Group (Firewall) erstellen
 # Hinweis: SSH (Port 22) ist absichtlich nicht konfiguriert für maximale Sicherheit.
 resource "exoscale_security_group" "web" {
-  name        = "web-server-sg"
+  name        = "jschurian-security-policies"
   description = "Erlaubt reinen Web-Traffic (HTTP und HTTPS)"
 }
- 
+
 # 2.1 HTTP erlauben (Port 80)
 resource "exoscale_security_group_rule" "http" {
   security_group_id = exoscale_security_group.web.id
@@ -40,7 +40,7 @@ resource "exoscale_security_group_rule" "http" {
   start_port        = 80
   end_port          = 80
 }
- 
+
 # 2.2 HTTPS erlauben (Port 443)
 resource "exoscale_security_group_rule" "https" {
   security_group_id = exoscale_security_group.web.id
@@ -50,19 +50,20 @@ resource "exoscale_security_group_rule" "https" {
   start_port        = 443
   end_port          = 443
 }
- 
+
 # 3. Die virtuelle Maschine (Compute Instance) erstellen
 resource "exoscale_compute_instance" "web_server" {
   zone               = local.zone
-  name               = "nginx-dashboard-server"
-  template_id        = data.exoscale_compute_template.ubuntu.id
+  name               = "Johannes-Schurian NGINX-Web-Server"
+  template_id        = data.exoscale_template.ubuntu.id
   type               = "standard.micro" 
   disk_size          = 10               
   security_group_ids = [exoscale_security_group.web.id]
-  # Hier wird unser Bash-Skript eingelesen und an die VM übergeben
-  user_data = file("${path.module}/cloud-init.sh")
+  
+  # Hier wird unser Cloud-Init Skript eingelesen und an die VM übergeben
+  user_data = file("${path.module}/cloud-init.yml")
 }
- 
+
 # 4. Ausgabe der öffentlichen IP-Adresse nach dem Deployment
 output "server_ip" {
   value       = exoscale_compute_instance.web_server.public_ip_address
--- Schurian_Johannes/Abgabe_2_TERRAFORM/variables.tf
+++
@@ -1,9 +0,0 @@
-# ============================================================
-# variables.tf – Eingabevariablen für die Terraform Konfiguration
-# ============================================================
-
-variable "zone" {
-  description = "Exoscale Zone, in der die VM erstellt wird"
-  type        = string
-  default     = "at-vie-1"
-}
