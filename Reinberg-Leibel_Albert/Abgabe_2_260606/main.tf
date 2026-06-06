c915094602d62a17deca900c8594a99d11d02420
Magitor-RGB <albert.leibel@gmail.com>
2026-06-06 20:03:02 +0200

    general fixes in reinberg-leibel_deploy.yaml and main.tf

--- Reinberg-Leibel_Albert/Abgabe_2_260606/main.tf
+++ Reinberg-Leibel_Albert/Abgabe_2_260606/main.tf
@@ -18,11 +18,11 @@ locals {
   template   = "Linux Ubuntu 26.04 LTS 64-bit"  
 }
 
-# 1. Das aktuellste Ubuntu-Template in der gewählten Zone suchen
-data "exoscale_compute_template" "ubuntu" {
-  zone = local.zone
-  name = local.template
-}
+## 1. Das aktuellste Ubuntu-Template in der gewählten Zone suchen
+#data "exoscale_compute_template" "ubuntu" {
+#  zone = local.zone
+#  name = local.template
+#}
 
 # 2. Security Group (Firewall) erstellen
 # Hinweis: SSH (Port 22) ist absichtlich nicht konfiguriert für maximale Sicherheit.
