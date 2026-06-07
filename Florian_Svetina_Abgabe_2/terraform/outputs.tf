47c817eb5632187318db41f62d37bb843b790fb8
flofloflo-lab <svetinaflorian02@gmail.com>
2026-06-07 23:37:23 +0200

    Delete Florian_Svetina_Abgabe_2 directory

--- Florian_Svetina_Abgabe_2/terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "vm_public_ip" {
-    value = exoscale_compute_instance.vm.public_ip_address
-}
-
-output "vm_http_url" {
-    value = "http://${exoscale_compute_instance.vm.public_ip_address}"
-}
