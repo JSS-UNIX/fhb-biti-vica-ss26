93ab3ec05ab9a166977475396bc96aebed8e6dcf
pmen753 <2310640055@hochschule-burgenland.at>
2026-06-07 22:39:36 +0200

    Update Exoscale provider configuration in Terraform

--- Menghini_Patrick/Abgabe_2_300526/terraform/providers.tf
+++ Menghini_Patrick/Abgabe_2_300526/terraform/providers.tf
@@ -1,4 +1,12 @@
-# Exoscale Provider
+terraform {
+  required_providers {
+    exoscale = {
+      source  = "exoscale/exoscale"
+      version = "~> 0.64.0"
+    }
+  }
+}
+
 provider "exoscale" {
   key    = var.exoscale_api_key
   secret = var.exoscale_api_secret
