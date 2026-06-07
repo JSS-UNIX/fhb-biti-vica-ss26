d6e59c73644013b392252cf75631feabb5effce1
Konakci <2410640052@hochschule-burgenland.at>
2026-06-07 19:59:32 +0200

    Doppelte tf-Dateien entfernt

--- Abgabe_2_2026/terraform/provider.tf
+++
@@ -1,6 +0,0 @@
-# Der Exoscale Provider liest die Zugangsdaten aus Terraform-Variablen.
-# In GitHub Actions werden diese Werte über Repository Secrets gesetzt.
-provider "exoscale" {
-  key    = var.exoscale_api_key
-  secret = var.exoscale_api_secret
-}
