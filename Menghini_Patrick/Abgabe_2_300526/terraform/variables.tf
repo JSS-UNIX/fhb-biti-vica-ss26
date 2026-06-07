9ac861230f015470340ba194a05fadf75db4cd5a
pmen753 <2310640055@hochschule-burgenland.at>
2026-06-07 22:41:07 +0200

    Update variables.tf

--- Menghini_Patrick/Abgabe_2_300526/terraform/variables.tf
+++ Menghini_Patrick/Abgabe_2_300526/terraform/variables.tf
@@ -1,3 +1,13 @@
+variable "exoscale_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "exoscale_api_secret" {
+  type      = string
+  sensitive = true
+}
+
 variable "zone" {
   type    = string
   default = "at-vie-1"
